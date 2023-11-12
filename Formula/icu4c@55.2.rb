22075baf1cae5a48401ae3fbff5615e082d95ffc
Mr Chen <midoks@163.com>
2023-11-13 04:00:29 +0800

    Update icu4c@55.2.rb

--- Formula/icu4c@55.2.rb
+++ Formula/icu4c@55.2.rb
@@ -3,7 +3,7 @@ class Icu4c < Formula
   homepage "https://icu.unicode.org/home"
   url "https://github.com/unicode-org/icu/releases/download/release-55-2/icu4c-55_2-src.tgz"
   version "73.2"
-  md5sum "b960cd152fbb0f3fd2377f405712b198"
+  sha256 "eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759"
   license "ICU"
 
   livecheck do
