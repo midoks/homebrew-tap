40f6d05164329eb53f60f4612d13231698462f5f
Mr Chen <midoks@163.com>
2023-11-13 03:58:13 +0800

    brew install midoks/tap/icu4c@55.2

---
+++ Formula/icu4c@55.2.rb
@@ -0,0 +1,42 @@
+class Icu4c < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "https://icu.unicode.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-55-2/icu4c-55_2-src.tgz"
+  version "73.2"
+  md5sum "b960cd152fbb0f3fd2377f405712b198"
+  license "ICU"
+
+  livecheck do
+    url :stable
+    regex(/^release[._-]v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.gsub("-", ".") }.compact
+    end
+  end
+
+  keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
+
+  def install
+    args = %w[
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
+
+    cd "source" do
+      system "./configure", *std_configure_args, *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    if File.exist? "/usr/share/dict/words"
+      system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+    else
+      (testpath/"hello").write "hello\nworld\n"
+      system "#{bin}/gendict", "--uchars", "hello", "dict"
+    end
+  end
+end
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 homebrew tap for ruby-build
 
 - brew install midoks/tap/openssl@1.0
+- brew install midoks/tap/icu4c@55.2
