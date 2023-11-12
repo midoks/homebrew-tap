b18333152bf7f36ecbf64cdbc9e93f8f74c1721f
Mr Chen <midoks@163.com>
2023-11-13 04:05:33 +0800

    Update icu4c@55.2.rb

--- Formula/icu4c@55.2.rb
+++ Formula/icu4c@55.2.rb
@@ -2,7 +2,7 @@ class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://icu.unicode.org/home"
   url "https://github.com/unicode-org/icu/releases/download/release-55-2/icu4c-55_2-src.tgz"
-  version "73.2"
+  version "55.2"
   sha256 "eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759"
   license "ICU"
 
